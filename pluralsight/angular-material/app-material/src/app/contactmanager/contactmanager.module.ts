cc5739e0a4d38ca0e6568ede24d24445657d5d1f
Deepa <deepashri.cg@gmail.com>
2020-01-17 21:26:52 -0500

    Add material imports to contact manager module

--- pluralsight/angular-material/app-material/src/app/contactmanager/contactmanager.module.ts
+++ pluralsight/angular-material/app-material/src/app/contactmanager/contactmanager.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ContactmanagerAppComponent } from './contactmanager-app.component';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { MainContentComponent } from './components/main-content/main-content.component';
-import { SidenavComponent } from './components/sidenav/sidenav.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
+import {MaterialModule} from '../shared/material.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {FormsModule} from '@angular/forms';
+
+import {ContactmanagerAppComponent} from './contactmanager-app.component';
+import {ToolbarComponent} from './components/toolbar/toolbar.component';
+import {MainContentComponent} from './components/main-content/main-content.component';
+import {SidenavComponent} from './components/sidenav/sidenav.component';
 
 
 @NgModule({
   declarations: [ContactmanagerAppComponent, ToolbarComponent, MainContentComponent, SidenavComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule
   ]
 })
 export class ContactmanagerModule { }
