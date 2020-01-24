bd5d82d91b81793af1d0e8dbb987c4eb04c3dd75
Deepa <deepashri.cg@gmail.com>
2020-01-23 23:13:47 -0500

    Inject test service in the constructor
    This is the right way of injecting the service into the component

--- java-brains/angular-basics/third-project/src/app/app.component.ts
+++ java-brains/angular-basics/third-project/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {TestService} from './services/test.service';
 
 @Component({
@@ -9,10 +9,9 @@ import {TestService} from './services/test.service';
 export class AppComponent {
   title = 'third-project';
 
-  constructor() {
-    const svc = new TestService();
-    svc.printToConsole('hello');
-
+  // Test service is injected here
+  constructor(service: TestService) {
+    service.printToConsole('Got the service');
   }
 
 }
