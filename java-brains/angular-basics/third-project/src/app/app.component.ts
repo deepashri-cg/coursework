065c2e5b501e12550cdf3f717e6b389406a3c150
Deepa <deepashri.cg@gmail.com>
2020-01-23 22:06:52 -0500

    Creating an instance of the service
    This is not recommended. We can improve this later.

--- java-brains/angular-basics/third-project/src/app/app.component.ts
+++ java-brains/angular-basics/third-project/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TestService} from './services/test.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'third-project';
+
+  constructor() {
+    const svc = new TestService();
+    svc.printToConsole('hello');
+
+  }
+
 }
