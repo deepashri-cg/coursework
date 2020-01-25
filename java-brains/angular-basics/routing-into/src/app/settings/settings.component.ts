130a3cccc054a8562c6cc51f4212944126284904
Deepa <deepashri.cg@gmail.com>
2020-01-25 18:22:26 -0500

    Configure settings component routes dynamically

--- java-brains/angular-basics/routing-into/src/app/settings/settings.component.ts
+++ java-brains/angular-basics/routing-into/src/app/settings/settings.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-settings',
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  routes = [
+    {linkName: 'Profile', url: 'profile'},
+    {linkName: 'Contact', url: 'contact'}
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
