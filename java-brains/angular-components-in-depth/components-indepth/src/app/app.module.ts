e1a5d2224c28c5ae8ab5a95102e1358e3560a3f8
Deepa <deepashri.cg@gmail.com>
2020-01-25 18:55:22 -0500

    generate new component blog-post-tile

--- java-brains/angular-components-in-depth/components-indepth/src/app/app.module.ts
+++ java-brains/angular-components-in-depth/components-indepth/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BlogPostTileComponent } from './blog-post-tile/blog-post-tile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogPostTileComponent
   ],
   imports: [
     BrowserModule
