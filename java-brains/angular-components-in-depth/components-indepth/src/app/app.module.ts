5c45c3834f6cad16d6889202527cc154f90cb2bc
Deepa <deepashri.cg@gmail.com>
2020-01-25 18:58:39 -0500

    Generate new blog-list component

--- java-brains/angular-components-in-depth/components-indepth/src/app/app.module.ts
+++ java-brains/angular-components-in-depth/components-indepth/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BlogPostTileComponent } from './blog-post-tile/blog-post-tile.component';
+import { BlogListComponent } from './blog-list/blog-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlogPostTileComponent
+    BlogPostTileComponent,
+    BlogListComponent
   ],
   imports: [
     BrowserModule
