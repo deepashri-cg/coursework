9bb6797a0ba199eeb6a0a5132c7963d1d108bfbe
Deepa <deepashri.cg@gmail.com>
2020-01-25 11:36:32 -0500

    Make a GET request using HttpClient service

--- java-brains/angular-basics/third-project/src/app/app.component.ts
+++ java-brains/angular-basics/third-project/src/app/app.component.ts
@@ -1,15 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TestService} from './services/test.service';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   // Test service is injected here
-  constructor(private service: TestService) {
+  constructor(private service: TestService, private http: HttpClient) {
     this.service.printToConsole('Got the service');
+  }
+
+  ngOnInit() {
+    this.http.get('https://api.github.com/users/deepashri-cg');
   }
 
 }
