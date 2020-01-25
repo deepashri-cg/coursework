4541c716658ae6ca7e0d01953aceb1cf3e5495fd
Deepa <deepashri.cg@gmail.com>
2020-01-25 17:33:24 -0500

    Set up a default route

--- java-brains/angular-basics/routing-into/src/app/app-routing.module.ts
+++ java-brains/angular-basics/routing-into/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {SettingsComponent} from './settings/settings.component';
 
 
 const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'settings', component: SettingsComponent}
 ];
