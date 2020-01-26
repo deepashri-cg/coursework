e1d69263f06129b788f36593f82eff779a2ec00f
Deepa <deepashri.cg@gmail.com>
2020-01-25 22:13:27 -0500

    use member variables for title and summary

--- java-brains/angular-components-in-depth/components-indepth/src/app/blog-post-tile/blog-post-tile.component.ts
+++ java-brains/angular-components-in-depth/components-indepth/src/app/blog-post-tile/blog-post-tile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-blog-post-tile',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogPostTileComponent implements OnInit {
 
-  constructor() { }
+  title: string;
+  summary: string;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.title = 'Blog Title';
+    this.summary = 'Blog post summary';
   }
 
 }
