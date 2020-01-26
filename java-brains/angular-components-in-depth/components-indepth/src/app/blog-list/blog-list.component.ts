7503a880cae9ba9e6e6993a0f674764bca63d355
Deepa <deepashri.cg@gmail.com>
2020-01-26 17:29:44 -0500

    Add an array of array blog posts
    One array for the page, another array for the blog post on the page

--- java-brains/angular-components-in-depth/components-indepth/src/app/blog-list/blog-list.component.ts
+++ java-brains/angular-components-in-depth/components-indepth/src/app/blog-list/blog-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {BlogPost} from '../blog-post';
 
 @Component({
@@ -7,13 +7,68 @@ import {BlogPost} from '../blog-post';
   styleUrls: ['./blog-list.component.scss']
 })
 export class BlogListComponent implements OnInit {
-  blogPosts: BlogPost[] = [];
-  constructor() { }
+  blogPosts: BlogPost[][];
+
+  constructor() {
+  }
 
   ngOnInit() {
-    this.blogPosts.push(new BlogPost('Blog Post 1', 'Summary 1'));
-    this.blogPosts.push(new BlogPost('Blog Post 2', 'Lorem ipsum dolor sit amet orci aliquam.'));
-    this.blogPosts.push(new BlogPost('Blog Post 3', 'Summary 3'));
+    this.blogPosts = [
+      [
+        {
+          title: 'Blog Post 1',
+          summary: 'Summary 1'
+        },
+        {
+          title: 'Blog Post 2',
+          summary: 'Lorem ipsum dolor sit amet orci aliquam.'
+        },
+        {
+          title: 'Blog Post 3',
+          summary: 'Summary 3'
+        },
+        {
+          title: 'Blog Post 4',
+          summary: 'Summary 4'
+        }
+      ],
+      [
+        {
+          title: 'Blog Post 5',
+          summary: 'Summary 5'
+        },
+        {
+          title: 'Blog Post 6',
+          summary: 'Lorem ipsum dolor sit amet orci aliquam.'
+        },
+        {
+          title: 'Blog Post 7',
+          summary: 'Summary 7'
+        },
+        {
+          title: 'Blog Post 8',
+          summary: 'Summary 8'
+        }
+      ],
+      [
+        {
+          title: 'Blog Post 9',
+          summary: 'Summary 9'
+        },
+        {
+          title: 'Blog Post 10',
+          summary: 'Lorem ipsum dolor sit amet orci aliquam.'
+        },
+        {
+          title: 'Blog Post 11',
+          summary: 'Summary 11'
+        },
+        {
+          title: 'Blog Post 12',
+          summary: 'Summary 12'
+        }
+      ]
+    ];
   }
 
 }
