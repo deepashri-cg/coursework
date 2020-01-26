16d4555bfbb06767363082e880f58fe3d1d14b17
Deepa <deepashri.cg@gmail.com>
2020-01-26 17:39:53 -0500

    Create noOfPages input and pass the blogPost length

--- java-brains/angular-components-in-depth/components-indepth/src/app/paginator/paginator.component.ts
+++ java-brains/angular-components-in-depth/components-indepth/src/app/paginator/paginator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-paginator',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./paginator.component.scss']
 })
 export class PaginatorComponent implements OnInit {
+  @Input() noOfPages;
 
   constructor() { }
 
