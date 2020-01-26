f6579852ba2f796b53b2117eb3e71fc681648ea1
Deepa <deepashri.cg@gmail.com>
2020-01-25 22:19:09 -0500

    declare title and summary as inputs

--- java-brains/angular-components-in-depth/components-indepth/src/app/blog-post-tile/blog-post-tile.component.ts
+++ java-brains/angular-components-in-depth/components-indepth/src/app/blog-post-tile/blog-post-tile.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-blog-post-tile',
@@ -7,8 +7,8 @@ import {Component, OnInit} from '@angular/core';
 })
 export class BlogPostTileComponent implements OnInit {
 
-  title: string;
-  summary: string;
+  @Input() title: string;
+  @Input() summary: string;
 
   constructor() {
   }
