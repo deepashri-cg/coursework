4e3c9950b09e7fe82ac8c0409e9d0d4bd71891d3
Deepa <deepashri.cg@gmail.com>
2020-01-26 20:32:16 -0500

    move the data to the service
    Call the getData from the service to populate the component

--- java-brains/angular-components-in-depth/components-indepth/src/app/blog-list/blog-list.component.ts
+++ java-brains/angular-components-in-depth/components-indepth/src/app/blog-list/blog-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, QueryList, ViewChildren} from '@angular/core';
 import {BlogPost} from '../blog-post';
 import {BlogPostTileComponent} from '../blog-post-tile/blog-post-tile.component';
+import {BlogDataService} from '../services/blog-data.service';
 
 @Component({
   selector: 'app-blog-list',
@@ -12,67 +13,12 @@ export class BlogListComponent implements OnInit {
   currentPage: number;
   @ViewChildren('tile') blogPostTileComponents: QueryList<BlogPostTileComponent>;
 
-  constructor() {
+  constructor(private blogDataService: BlogDataService) {
   }
 
   ngOnInit() {
     this.currentPage = 0;
-    this.blogPosts = [
-      [
-        {
-          title: 'Blog Post 1',
-          summary: 'Summary 1 Lorem ipsum dolor sit amet orci aliquam.'
-        },
-        {
-          title: 'Blog Post 2',
-          summary: 'Lorem ipsum dolor sit amet orci aliquam.'
-        },
-        {
-          title: 'Blog Post 3',
-          summary: 'Summary 3'
-        },
-        {
-          title: 'Blog Post 4',
-          summary: 'Summary 4'
-        }
-      ],
-      [
-        {
-          title: 'Blog Post 5',
-          summary: 'Summary 5'
-        },
-        {
-          title: 'Blog Post 6',
-          summary: 'Lorem ipsum dolor sit amet orci aliquam.'
-        },
-        {
-          title: 'Blog Post 7',
-          summary: 'Summary 7'
-        },
-        {
-          title: 'Blog Post 8',
-          summary: 'Summary 8'
-        }
-      ],
-      [
-        {
-          title: 'Blog Post 9',
-          summary: 'Summary 9'
-        },
-        {
-          title: 'Blog Post 10',
-          summary: 'Lorem ipsum dolor sit amet orci aliquam.'
-        },
-        {
-          title: 'Blog Post 11',
-          summary: 'Summary 11'
-        },
-        {
-          title: 'Blog Post 12',
-          summary: 'Summary 12'
-        }
-      ]
-    ];
+    this.blogPosts = this.blogDataService.getData();
   }
 
   updatePage(newPage) {
