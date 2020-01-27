70947a60109817410e33635226da5d5f476ebb52
Deepa <deepashri.cg@gmail.com>
2020-01-26 22:54:31 -0500

    pass parameters to the directive

--- java-brains/angular-components-in-depth/components-indepth/src/app/directives/highlight.directive.ts
+++ java-brains/angular-components-in-depth/components-indepth/src/app/directives/highlight.directive.ts
@@ -1,15 +1,17 @@
-import {Directive, ElementRef, HostListener} from '@angular/core';
+import {Directive, ElementRef, HostListener, Input} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
+  @Input('appHighlight') color = 'yellow';
+
   constructor(private element: ElementRef) {
   }
 
   @HostListener('mouseenter') addHighLight() {
-    this.element.nativeElement.style.backgroundColor = 'yellow';
+    this.element.nativeElement.style.backgroundColor = this.color;
   }
 
   @HostListener('mouseleave') removeHighLight() {
