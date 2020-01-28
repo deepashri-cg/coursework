38bc64b80a5b0266f98d5b8502c50d01bff2f398
Deepa <deepashri.cg@gmail.com>
2020-01-27 21:09:28 -0500

    import forms module

--- tutsplus/angular-hands-on-registration-form/registration-app/src/app/app.module.ts
+++ tutsplus/angular-hands-on-registration-form/registration-app/src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { RegistrationFormComponent } from './registration-form/registration-form.component';
+import {AppComponent} from './app.component';
+import {RegistrationFormComponent} from './registration-form/registration-form.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,9 +11,11 @@ import { RegistrationFormComponent } from './registration-form/registration-form
     RegistrationFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
