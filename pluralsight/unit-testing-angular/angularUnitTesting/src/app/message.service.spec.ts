5685038dba955cebd9b12ec51296395ad54b621d
Deepa <deepashri.cg@gmail.com>
2020-02-02 00:53:27 -0500

    unit tests for message service

--- pluralsight/unit-testing-angular/angularUnitTesting/src/app/message.service.spec.ts
+++ pluralsight/unit-testing-angular/angularUnitTesting/src/app/message.service.spec.ts
@@ -7,4 +7,22 @@ describe('Message Service', () => {
     service = new MessageService();
   });
 
+  it('should have no messages to start', () => {
+    expect(service.messages.length).toBe(0);
+  });
+
+  it('should add a message when add is called', () => {
+    service.add('message1');
+
+    expect(service.messages.length).toBe(1);
+  });
+
+  it('should remove all messages when clear is called', () => {
+    service.add('message1');
+
+    service.clear();
+
+    expect(service.messages.length).toBe(0);
+  });
+
 });
