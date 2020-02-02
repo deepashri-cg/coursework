9d9d2766509eeb4aa11fa459732ff9ae398aa617
Deepa <deepashri.cg@gmail.com>
2020-02-01 21:57:37 -0500

    using content child decorator to get the heading element in the shell component

--- java-brains/angular-components-in-depth/components-indepth/src/app/shell/shell.component.ts
+++ java-brains/angular-components-in-depth/components-indepth/src/app/shell/shell.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ContentChild, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-shell',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShellComponent implements OnInit {
 
-  constructor() { }
+  @ContentChild('appHeader', {static: true}) headerElement;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    console.log(this.headerElement);
   }
 
 }
