d831426078f8ccd64ea98b0dc61d305b69417907
Deepa <deepashri.cg@gmail.com>
2020-02-02 19:36:37 -0500

    hero component shallow unit test
    Usage of TestBed and fixtures

---
+++ pluralsight/unit-testing-angular/angularUnitTesting/src/app/hero/hero.component.shallow.spec.ts
@@ -0,0 +1,19 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {HeroComponent} from './hero.component';
+
+describe('HeroComponent (shallow tests)', () => {
+  let fixture: ComponentFixture<HeroComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeroComponent]
+    });
+    fixture = TestBed.createComponent(HeroComponent);
+  });
+
+  it('should have the correct hero', () => {
+    fixture.componentInstance.hero = {id: 1, name: 'Super Dude', strength: 3};
+
+    expect(fixture.componentInstance.hero.name).toEqual('Super Dude');
+  });
+});
