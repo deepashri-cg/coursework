64e8f418675732547be0ba5055b58db4cc3952f5
Deepa <deepashri.cg@gmail.com>
2020-02-02 19:44:31 -0500

    add no error schema declaration

--- pluralsight/unit-testing-angular/angularUnitTesting/src/app/hero/hero.component.shallow.spec.ts
+++ pluralsight/unit-testing-angular/angularUnitTesting/src/app/hero/hero.component.shallow.spec.ts
@@ -1,12 +1,14 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {HeroComponent} from './hero.component';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
 
 describe('HeroComponent (shallow tests)', () => {
   let fixture: ComponentFixture<HeroComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HeroComponent]
+      declarations: [HeroComponent],
+      schemas: [NO_ERRORS_SCHEMA]
     });
     fixture = TestBed.createComponent(HeroComponent);
   });
