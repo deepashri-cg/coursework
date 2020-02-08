ae0a581cd44eed910a19a20c796d62cdbf3525a8
Deepa <deepashri.cg@gmail.com>
2020-02-08 09:00:54 -0500

    add test to make sure the heroes component is getting 3 heroes from the service

--- pluralsight/unit-testing-angular/angularUnitTesting/src/app/heroes/heroes.component.shallow.spec.ts
+++ pluralsight/unit-testing-angular/angularUnitTesting/src/app/heroes/heroes.component.shallow.spec.ts
@@ -2,6 +2,7 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {HeroesComponent} from './heroes.component';
 import {NO_ERRORS_SCHEMA} from '@angular/core';
 import {HeroService} from '../hero.service';
+import {of} from 'rxjs';
 
 describe('HeroesComponent (shallow tests)', () => {
   let fixture: ComponentFixture<HeroesComponent>;
@@ -24,7 +25,10 @@ describe('HeroesComponent (shallow tests)', () => {
     });
     fixture = TestBed.createComponent(HeroesComponent);
   });
-  it('should do nothing', () => {
-    expect(true).toBe(true);
+  it('should set heroes correctly from the service', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+
+    expect(fixture.componentInstance.heroes.length).toBe(3);
   });
 });
