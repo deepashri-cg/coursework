566665344c052b200d245b34cc6b7a13ebd3c3ee
Deepa <deepashri.cg@gmail.com>
2020-02-08 08:40:20 -0500

    create mock hero service

--- pluralsight/unit-testing-angular/angularUnitTesting/src/app/heroes/heroes.component.shallow.spec.ts
+++ pluralsight/unit-testing-angular/angularUnitTesting/src/app/heroes/heroes.component.shallow.spec.ts
@@ -1,14 +1,18 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {HeroesComponent} from './heroes.component';
 import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {HeroService} from '../hero.service';
 
 describe('HeroesComponent (shallow tests)', () => {
   let fixture: ComponentFixture<HeroesComponent>;
-
+  let mockHeroService;
   beforeEach(() => {
+    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
     TestBed.configureTestingModule({
       declarations: [HeroesComponent],
-      providers: [],
+      providers: [
+        {provide: HeroService, useValue: mockHeroService}
+      ],
       schemas: [NO_ERRORS_SCHEMA]
     });
     fixture = TestBed.createComponent(HeroesComponent);
